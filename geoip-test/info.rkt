30a465b56f2c0ae0bfbcd02ad0a75e562969419a
Bogdan Popa <bogdan@defn.io>
2025-05-31 10:11:54 +0300

    build: update build deps

--- geoip-test/info.rkt
+++ geoip-test/info.rkt
@@ -3,7 +3,10 @@
 (define license 'BSD-3-Clause)
 (define collection 'multi)
 (define deps '())
-(define build-deps '("base"
-                     "geoip-lib"
-                     "rackunit-lib"))
-(define update-implies '("geoip-lib"))
+(define build-deps
+  '("base"
+    "geoip-lib"
+    "rackcheck-lib"
+    "rackunit-lib"))
+(define update-implies
+  '("geoip-lib"))
